a758079dfe0321cb27391c6d1e68152d1fb58bb0
nilay <mailnilaydas@gmail.com>
2021-11-29 09:54:28 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    private final Environment environment;
+
+    @Autowired
+    public HomeController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @RequestMapping("/")
+    public ResponseEntity<?> getStatus()
+    {
+        return ResponseEntity.ok("order-ws is up and running on port: "+environment.getProperty("local.server.port"));
+    }
+}
---
+++ src/main/java/com/example/demo/entity/OrderEntity.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public class OrderEntity {
+
+    private Integer orderId;
+    private String id;
+    private String orderName;
+    private Double orderPrice;
+}
