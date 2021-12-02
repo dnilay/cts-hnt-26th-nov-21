9104a6e054dd8c05d7a2f9706c8a5d0335df8be6
nilay <mailnilaydas@gmail.com>
2021-12-02 12:13:12 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,42 +3,38 @@ import React,{Component} from "react";
 
 class App extends Component
 {
-  constructor(props) {
-
-    super(props);
-    this.state={count:0};
-    console.log('constructor',this.state);
-  }
-  handleClick(event)
-  {
-
-    //alert(event.target.valueOf());
-    const axios=require('axios');
-      axios.get('http://54.179.218.221:8081/todos')
-          .then(res=>{
-              console.log(res.data);
-          }).catch(error=>{
-          console.error('Error',error);
-      });
-  }
-  handlePostClick(event)
-  {
-    console.log('handlePostClick');
-      const axios=require('axios');
-    axios.post('http://54.179.218.221:8081/todos',{name:'packaging'})
-        .then(res=>{
-           console.log(res.data);
-        }).catch(err=>{
-            console.error('ERROR',err);
-    });
-  }
-render() {
-    return(
-        <div className="App">
-          <button onClick={this.handleClick.bind(this)} name="mybutton" value="test">Get All ToDos</button>
-          <button onClick={this.handlePostClick.bind(this)}>Create New ToDo</button>
-        </div>
-    );
+    constructor(props) {
+        super(props)
+        this.state ={ todos: []};
+        console.log('constructor');
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+        const axios = require('axios');
+        axios.get('http://localhost:8081/todos/')
+            .then(res=>{
+                this.setState({todos : res.data});
+            }).catch(error=>{
+            console.error('Error',error)
+        })
+    }
+componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate');
 }
+
+    render() {
+        let liElements = [];
+        for(let todo of this.state.todos){
+            liElements.push(<li>{todo.name}</li>);
+        }
+        return (
+            <div className="App">
+                <ul>{liElements}</ul>
+            </div>
+        );
+    }
+
 }
+
+
 export default App
