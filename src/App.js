d248f83ac2030f8ae4c448512fb3e5d21fe55c79
nilay <mailnilaydas@gmail.com>
2021-12-03 11:56:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {Route,Switch,Link} from "react-router-dom";
+import GetAll from "./GetAll";
+import Create from "./Create";
+import Get from "./Get";
+import Update from "./Update"
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                <Switch>
+                    <Route exact path="/" component={GetAll}/>
+                    <Route path="/Create" component={Create}/>
+                </Switch>
+                <Link to="/Create">Create</Link><br/>
+
+            </div>
+        );
+    }
 }
 
 export default App;
