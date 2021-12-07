a91a2923147af63bcb939cb5907ba3b9d069cb1c
nilay <mailnilaydas@gmail.com>
2021-12-07 12:41:42 +0530

    final commit

---
+++ src/test/java/com/example/beanstalkdemo/BeanstalkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beanstalkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
