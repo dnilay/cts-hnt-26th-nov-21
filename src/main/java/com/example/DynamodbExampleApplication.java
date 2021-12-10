b67b11d5327bcce5bcc788b903824b72cceb592f
nilay <mailnilaydas@gmail.com>
2021-12-10 09:49:47 +0530

    first commit

---
+++ src/main/java/com/example/DynamodbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamodbExampleApplication.class, args);
+    }
+
+}
